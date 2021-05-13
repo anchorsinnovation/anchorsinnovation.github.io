7a9f1ed74a76be9f530a5e6d2c0721b02654756c
anchorsadmin <77377356+anchorsadmin@users.noreply.github.com>
2021-05-13 13:43:48 +0930

    updated script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+const loader = document.getElementById("loader")
 const navbar = document.getElementById('navbar')
 const menuToggle = document.getElementById('navbarSupportedContent')
 const navbarTogglerIcon = document.getElementById('navbar-toggler-icon')
 const backToTopButton = document.getElementById('back-to-top-button')
+
+//Loading screen transition
+function showPage() {
+  loader.classList.remove('d-flex')
+  loader.classList.add('d-none')
+}
+function loadFunction() {
+  setTimeout(showPage, 1500)
+}
 
 /*
 When the user scrolls down from the top of the document,
@@ -9,10 +19,16 @@ change navbar color and show back-to-top button
 */
 window.addEventListener('scroll', function() {
   if (menuToggle.classList.contains('show') === false) {
-    navbar.classList.toggle('showNavbar', window.pageYOffset > 0)
+    navbar.classList.remove('hideNavbar', window.pageYOffset > 0)
     navbarTogglerIcon.classList.remove('rotate')
   }
-  backToTopButton.classList.toggle('pageScrolled', window.pageYOffset > 0)
+  if (window.pageYOffset === 0) {
+    backToTopButton.classList.add('d-none')
+    navbar.classList.add ('hideNavbar')
+  } else {
+    backToTopButton.classList.remove('d-none')
+    navbar.classList.remove('hideNavbar')
+  }
 })
 
 /*
@@ -21,10 +37,10 @@ navbar button will change menu color when expanded
 */
 function colorChangeFunction() {
   if (window.pageYOffset === 0) {
-    if (navbar.classList.contains('showNavbar') === false) {
-      navbar.classList.add ('showNavbar')
-    } else if (navbar.classList.contains('showNavbar') === true) {
-      navbar.classList.remove('showNavbar')
+    if (navbar.classList.contains('hideNavbar') === false) {
+      navbar.classList.add ('hideNavbar')
+    } else if (navbar.classList.contains('hideNavbar') === true) {
+      navbar.classList.remove('hideNavbar')
     }
   }
 }
@@ -34,19 +50,25 @@ function closeFunction() {
   menuToggle.classList.remove('show')
   navbarTogglerIcon.classList.remove('rotate')
   if (window.pageYOffset === 0) {
-    navbar.classList.remove('showNavbar')
+    navbar.classList.add('hideNavbar')
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// animation of navbar button
 function rotateFunction() {
   navbarTogglerIcon.classList.toggle('rotate')
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// When the user clicks the back-to-top buttion, reset navbar button
 function topFunction() {
   menuToggle.classList.remove('show')
   navbarTogglerIcon.classList.remove('rotate')
-  document.body.scrollTop = 0
-  document.documentElement.scrollTop = 0
+}
+
+// Self executing function
+loader.classList.remove('d-none')
+loader.classList.add('d-flex')
+if (window.pageYOffset === 0) {
+  backToTopButton.classList.add('d-none')
+  navbar.classList.add ('hideNavbar')
 }
